34a5cba367291d388fca44b50190d08781ac1eb1
HinaOhsawa <strobe.hina@gmail.com>
2025-06-29 19:11:35 +0900

    国一覧を作成

---
+++ src/components/CountryList.tsx
@@ -0,0 +1,55 @@
+import Flag from "react-world-flags";
+import country from "../data/country.json";
+import type { Question } from "../types/questions";
+
+const regionOrder = [
+  "North America",
+  "South America",
+  "Europe",
+  "Africa",
+  "Asia",
+  "Australia",
+];
+
+// regionごとに国をグループ化するヘルパー関数
+const groupByRegion = (countries: Question[]) => {
+  return countries.reduce<Record<string, Question[]>>((acc, curr) => {
+    if (!acc[curr.region]) acc[curr.region] = [];
+    acc[curr.region].push(curr);
+    return acc;
+  }, {});
+};
+
+export default function CountryList() {
+  const grouped = groupByRegion(country);
+
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+      <p className="text-lg font-bold">
+        以下{country.length}カ国の中から出題されます
+      </p>
+      {regionOrder.map((region) => {
+        const countries = grouped[region];
+        if (!countries) return null; // その地域に国がなければスキップ
+
+        return (
+          <section key={region} className="mb-6">
+            <h2 className="text-xl text-left font-semibold mb-3">{region}</h2>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+              {countries.map((item: Question) => (
+                <div
+                  key={item.code}
+                  className="p-3 border border-stone-400 rounded text-center"
+                >
+                  <p className="text-sm font-bold mb-2">{item.display_name}</p>
+                  <Flag className="m-auto h-20 shadow-md" code={item.code} />
+                </div>
+              ))}
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import CountryList from "../components/CountryList";
 
 export default function HomePage() {
   return (
     <main>
-      <p className="font-bold text-3xl font-kaisei mb-2 ">国歌当てクイズ</p>
-
       <div className="flex  justify-center">
         <img src="images/icon-world_color.png" alt="" className="h-10 mr-2" />
         <h1 className="text-4xl logo mb-2">Anthem Club</h1>
         <img src="images/icon-hachibuonpu.png" alt="" className="h-10" />
       </div>
+
+      <p className="font-bold text-3xl font-kaisei mb-2 ">国歌当てクイズ</p>
       <p className="text-md font-bold">
         国歌の音声を聞いて、選択肢から正しい国を当てるクイズゲームです。
       </p>
@@ -30,6 +31,8 @@ export default function HomePage() {
           style={{ transform: "scaleX(-1)" }}
         />
       </div>
+
+      <CountryList />
     </main>
   );
 }
