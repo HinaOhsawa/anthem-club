97cbe64756b5c590deacb7571fb75b24154bb661
HinaOhsawa <strobe.hina@gmail.com>
2025-06-30 12:14:54 +0900

    ページ遷移したらページトップにスクロール

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import QuizPage from "./pages/QuizPage";
 import ResultPage from "./pages/ResultPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <ScrollToTop />
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
